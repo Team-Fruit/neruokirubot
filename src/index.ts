0023fabc987f3e7c4f8ad4bac58a0da9d9de41e4
kokoa0429 <kokoa@ciebus.net>
2021-01-27 15:47:45 +0900

    fix バグ

--- src/index.ts
+++ src/index.ts
@@ -114,9 +114,9 @@ function initMsg() {
 }
 
 function getTimeFromMills(m: number) {
-  let byo: number = Math.floor(m % 1000);
-  let hun: number = Math.floor(m % 60000);
-  let ji: number = Math.floor(m % 3600000);
+  let byo: number = Math.floor(m / 1000) % 60;
+  let hun: number = Math.floor(m / 60000) % 60;
+  let ji: number = Math.floor(m / 3600000);
   let result = "";
   if (ji != 0) result += ji + "時間 ";
   if (hun != 0) result += hun + "分 ";
