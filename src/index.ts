be5982ebeece0c6eb82286e74bb1b23bbe80acb5
kokoa0429 <kokoa@ciebus.net>
2021-02-01 00:56:05 +0900

    コメントつけた

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,38 @@ import { Connection, ConnectionOptions, createConnection } from "typeorm";
 import { User } from "./entity/User";
 import { Sleep } from "./entity/Sleep";
 
+// Discordクライアント
 const client = new Client();
 
+// TypeORMのオプション
 const options: ConnectionOptions = {
   type: "sqlite",
-  database: "../db/db.sqlite3",
+  database: "./db/db.sqlite3",
   entities: [User, Sleep],
-  synchronize: true,
+  synchronize: false,
 };
 
+// TypeORMのコネクション 使う前にnullチェックが必要
 let connection: Connection | null = null;
 
 async function connectDB() {
   connection = await createConnection(options);
   const userRepository = connection.getRepository(User);
   console.log(await userRepository.count());
+  await connection.query("PRAGMA foreign_keys=OFF");
+  await connection.synchronize();
+  await connection.query("PRAGMA foreign_keys=ON");
 }
 
+// コネクションする
 connectDB();
 
+// DiscordBotがいい感じになったとき
 client.on("ready", () => {
   console.log("Discord Bot Ready");
 });
 
+// いろんな定数 TODO コンフィグ化
 const neru = "<:ne:803311475502350398>";
 const neruEmojiID = "803311475502350398";
 const okiru = "<:ki:803311475325796434>";
@@ -39,23 +48,28 @@ const generalChannel = "606109479003750442";
 
 const guildID = "606109479003750440";
 
+// 今リアクションを待機してるメッセージ
 let nowMessage: Message | null = null;
 
+// いずれ消えるもの
 let sleepTime: Map<string, ISleep> = new Map();
 
+// リアクションされたときのインターフェース
 interface IReaction {
-  user_id: string;
-  message_id: string;
-  emoji: Emoji;
-  channel_id: string;
-  guild_id: string;
+  user_id: string; // Discord uid
+  message_id: string; // Message id
+  emoji: Emoji; // EmojiResolvable
+  channel_id: string; // ChannelID
+  guild_id: string; // GuildID
 }
 
+// 寝てるオブジェクトのインターフェース いずれ消えるもの
 interface ISleep {
   action: string;
   date: number;
 }
 
+// Botのメッセージを初期化する 起きるリアクションと寝るリアクションをつける
 async function initBotMessage(msg: Message) {
   const msgRes = await (<TextChannel>client.channels.cache.get(noChannel)).send(
     "このメッセージにリアクションしてね(⋈◍＞◡＜◍)。✧♡"
@@ -66,6 +80,7 @@ async function initBotMessage(msg: Message) {
   await msg.delete();
 }
 
+// /no status コマンドの処理 TODO
 async function sendUserStatus(msg: Message) {
   let returnMsg = "";
   sleepTime.forEach((value, key) => {
@@ -83,6 +98,7 @@ async function sendUserStatus(msg: Message) {
   await c.send(returnMsg);
 }
 
+// テスト用リッチメッセージ
 async function test(msg: Message) {
   await msg.channel.send({
     embed: {
@@ -102,8 +118,11 @@ async function test(msg: Message) {
   });
 }
 
+// メッセージが来たとき
 client.on("message", async (msg) => {
   if (msg.author.bot) return;
+  if (!msg.content.startsWith("/no")) return;
+  // 引数をパース
   const args = msg.content.replace(/　+/g, " ").slice(3).trim().split(/ + /);
   switch (args[0]) {
     case "init":
@@ -118,8 +137,14 @@ client.on("message", async (msg) => {
   }
 });
 
+// メッセージにリアクションがあったとき
 async function msgReactionAdd(reaction: IReaction) {
+  let g = client.guilds.cache.get(reaction.guild_id);
+  const general = <TextChannel>g?.channels.cache.get(generalChannel); // TODO
+
+  if (g?.member(reaction.user_id)?.user.bot) return;
   const userRepository = connection?.getRepository(User);
+  const sleepRepository = connection?.getRepository(Sleep);
   const user = await userRepository?.findOne({ discordId: reaction.user_id });
   if (!user) {
     // Userが未登録だった時
@@ -128,61 +153,64 @@ async function msgReactionAdd(reaction: IReaction) {
     });
     await userRepository?.save(<User>newUser);
   } else {
+    switch (reaction.emoji.name) {
+      case "ne":
+        await userRepository?.update(
+          { discordId: reaction.user_id },
+          {
+            nowSleeping: true,
+            sleepTempTime: new Date().getTime(),
+          }
+        );
+        g?.member(reaction.user_id)?.roles.remove(okiruRole);
+        g?.member(reaction.user_id)?.roles.add(neruRole);
+        await general.send(
+          getNamefromID(reaction.user_id) + "はねました。ぽやしみ"
+        );
+        initMsg();
+        break;
+      case "ki":
+        g?.member(reaction.user_id)?.roles.remove(neruRole);
+        g?.member(reaction.user_id)?.roles.add(okiruRole);
+        if (user.nowSleeping) {
+          const date = new Date().getTime();
+          const sleep = sleepRepository?.create({
+            sleepTime: user.sleepTempTime,
+            wakeTime: date,
+            user: user,
+          });
+          sleepRepository?.save(<Sleep>sleep);
+          await general.send(
+            getNamefromID(reaction.user_id) +
+              "は" +
+              getTimeFromMills(date - user.sleepTempTime) +
+              " ねました"
+          );
+          userRepository?.update(
+            { discordId: reaction.user_id },
+            { nowSleeping: false }
+          );
+        }
+        initMsg();
+        break;
+    }
   }
-
-  /*
-  let g = client.guilds.cache.get(guildID);
-  const c = <TextChannel>g?.channels.cache.get(generalChannel);
-  let nickName = g?.member(reaction.user_id)?.nickname?.replace("@", "＠");
-  if (!nickName) nickName = g?.member(reaction.user_id)?.displayName;
-  if (g?.member(reaction.user_id)?.user.bot) return;
-  switch (reaction.emoji.name) {
-    case "ne":
-      g?.member(reaction.user_id)?.roles.remove(okiruRole);
-      g?.member(reaction.user_id)?.roles.add(neruRole);
-      updateUserStatus(reaction.user_id, "ne");
-      c.send(nickName + "はねました。ぽやしみ");
-      break;
-    case "ki":
-      g?.member(reaction.user_id)?.roles.remove(neruRole);
-      g?.member(reaction.user_id)?.roles.add(okiruRole);
-      const res = updateUserStatus(reaction.user_id, "ki");
-      console.log(res);
-      if (res) {
-        const netaTime = getTimeFromMills(res);
-        c.send(nickName + "は" + netaTime + "ねました。");
-      }
-      break;
-  }
-   */
-  initMsg();
 }
+
+// discord uid からニックネームをとってくるメソッド
+function getNamefromID(id: string) {
+  let g = client.guilds.cache.get(guildID);
+  let nickName = g?.member(id)?.nickname?.replace("@", "＠");
+  if (!nickName) nickName = g?.member(id)?.displayName;
+  return nickName;
+}
+
+// リアクションが消されたとき
 function msgReactionRemove(reaction: IReaction) {
   // return reaction;
 }
 
-function updateUserStatus(id: string, status: string) {
-  const s: ISleep = {
-    action: status,
-    date: new Date().getTime(),
-  };
-  if (sleepTime.has(id)) {
-    const b: ISleep = <ISleep>sleepTime.get(id);
-    if (b.action == "ne" && status == "ki") {
-      const now = new Date().getTime();
-      const diff = now - b.date;
-      sleepTime.set(id, s);
-      return diff;
-    } else {
-      sleepTime.set(id, s);
-      return null;
-    }
-  } else {
-    sleepTime.set(id, s);
-    return null;
-  }
-}
-
+// initMeg()
 function initMsg() {
   if (!nowMessage) {
     console.log("not initialized");
@@ -194,6 +222,7 @@ function initMsg() {
   });
 }
 
+// ミリ秒を x時間x分x秒にするやつ いらない単位は消える
 function getTimeFromMills(m: number) {
   let byo: number = Math.floor(m / 1000) % 60;
   let hun: number = Math.floor(m / 60000) % 60;
@@ -205,6 +234,7 @@ function getTimeFromMills(m: number) {
   return result;
 }
 
+// rawイベントを取得 リアクションのイベントを発火させてる
 client.on("raw", (reaction) => {
   switch (reaction.t) {
     case "MESSAGE_REACTION_ADD":
@@ -216,5 +246,7 @@ client.on("raw", (reaction) => {
   }
 });
 
+// tokenを環境変数から読み込み
 const token = process.env.D_TOKEN;
+// botにログイン
 client.login(token);
